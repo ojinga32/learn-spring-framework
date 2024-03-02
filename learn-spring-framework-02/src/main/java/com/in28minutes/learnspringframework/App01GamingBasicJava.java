9ed5812f462ee6cdec26b7eb9b07d39ba89df719
ojinga32 <ojinga32@gmail.com>
2024-03-02 23:55:26 +0900

    제발 올라가라

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+
+public class App01GamingBasicJava {
+
+	public static void main(String[] args) {
+		// 변경후
+		var game = new PacmanGame();	// 1: Object Creation
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+		var gameRunner = new GameRunner(game); 	
+		// 2: Object Creation + Wiring of Dependencies
+		// Game is a Dependency
+		gameRunner.run();
+	}
+
+}
---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole{
+
+	public void up() {
+		System.out.println("up");
+	}
+	
+	public void down() {
+		System.out.println("down");
+	}
+	
+	public void left() {
+		System.out.println("Go back");
+	}
+	
+	public void right() {
+		System.out.println("Accelerate");
+	}
+
+}
